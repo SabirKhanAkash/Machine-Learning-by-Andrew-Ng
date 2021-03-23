799712c897fcaa44e62916e28ebdd3244f507ae2
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-03-23 16:49:29 +0600

    Week 1 all remaining prac and quiz codes added

---
+++ Week1Practice6.m
@@ -0,0 +1,6 @@
+A = [1,2,0;0,5,6;7,0,9]
+A_Trans = A'
+
+A_Inv = inv(A)
+
+A_InvA = inv(A)*A
---
+++ Week1Quiz5.m
@@ -0,0 +1,8 @@
+A = [1,2,0;0,5,6;7,0,9]
+B = [1,3,0;0,7,6;7,0,4]
+
+mult_AB = A*B
+mult_BA = B*A
+
+mult_ABA = mult_AB*A
+mult_BAB = mult_BA*B
