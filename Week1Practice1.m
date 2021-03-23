2b62ccaa28b03ca02a3b12f4804277603c6c84da
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-03-23 16:14:19 +0600

    2 practice codes of week 1 are added

---
+++ Week1Practice1.m
@@ -0,0 +1,9 @@
+Matrix = [1,2,3; 4,5,6; 6,7,8; 5,6,3]
+Vector = [1;2;3;4]
+
+[m,n] = size(Matrix)
+dim_M = size(Matrix)
+
+dim_V = size(Vector)
+
+Mult_23 = Matrix(2,3)
---
+++ Week1Practice2.m
@@ -0,0 +1,13 @@
+A = [1,2,3;,4,5,6;6,7,8]
+B = [2,1,3;4,2,6;2,5,7]
+
+s = 2
+
+add_AB = A+B
+sub_AB = A-B
+
+mult_As = A*s
+
+div_As = A/s
+
+add_As = A+s
