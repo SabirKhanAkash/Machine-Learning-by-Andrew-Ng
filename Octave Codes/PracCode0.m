319d2d33b664b870aa6b77fc5d48e606b76d7273
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-06 23:08:33 +0600

    PracCode0 added to main

---
+++ Octave Codes/PracCode0.m
@@ -0,0 +1,49 @@
+5+3
+2^7
+
+1==2
+2<1
+1<2
+1==2
+7~=2
+1||0
+1&&0
+
+a = 2
+a = 3;
+b = 'hi'
+c = 1
+c = (3>=1)
+
+a = pi;
+a 
+
+disp(a)
+disp(sprintf('2 decimals: %0.2f',a))
+disp(sprintf('19 decimals: %0.19f',a))
+disp(sprintf('48 decimals: %0.48f',a))
+
+A = [1 2; 3 4; 5 6]
+v = [1 2 3]
+vc = [1; 2; 3]
+
+vc = 1:0.1:2
+vc = 1:6
+
+ones(2,3)
+
+c= 2*ones(2,3)
+
+w = zeros(1,3)
+
+w = rand(1,2)
+
+w = randn(1,2)
+
+w = -6 + sqrt(10)*(randn(1,10000))
+
+hist(w)
+hist(w,70)
+
+eye(4)
+I = eye(4)
