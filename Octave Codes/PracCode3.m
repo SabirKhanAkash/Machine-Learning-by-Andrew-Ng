5136f86edd9b09c34097b47441a511973e1e1121
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-07 22:53:26 +0600

    PracCode3 finished

--- Octave Codes/PracCode3.m
+++ Octave Codes/PracCode3.m
@@ -18,3 +18,24 @@ title('my plot');
 str = pwd
 
 print -dpng 'myPlot.png'
+figure(1); plot(t,y1);
+figure(2); plot(t,y2);
+
+subplot(1,2,1);
+plot(t,y1);
+
+subplot(1,2,2);
+plot(t,y2);
+
+axis([0.5 1 -1 1])
+clf;
+a = magic(5)
+
+imagesc(a)
+imagesc(a), colorbar, colormap gray;
+
+a(1,2)
+a(4,5)
+imagesc(magic(15)), colorbar, colormap gray;
+
+a =1, b=2, c=3
