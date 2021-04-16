f3630b3af31532d5383ee4a4dada6dbbf3da9791
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-16 22:55:45 +0600

    updated code plotData.m

--- Octave Codes/Week2-Programming-Assignments/plotData.m
+++ Octave Codes/Week2-Programming-Assignments/plotData.m
@@ -1,7 +1,4 @@
 function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
 
 figure; % open a new figure window
 
@@ -18,10 +15,6 @@ xlabel('Population of City in 10,000s');
 % Hint: You can use the 'rx' option with plot to have the markers
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
-
-
-
-
 
 % ============================================================
 
