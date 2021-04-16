05db2d2bd084555fbb2ea5d26e81bb7d75549162
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-16 22:51:03 +0600

    updated code computeCost.m

--- Octave Codes/Week2-Programming-Assignments/computeCost.m
+++ Octave Codes/Week2-Programming-Assignments/computeCost.m
@@ -1,9 +1,5 @@
 function J = computeCost(X, y, theta)
 
-% Initialize some useful values
-%m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
