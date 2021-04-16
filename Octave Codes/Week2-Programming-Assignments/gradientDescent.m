eb000f3cf19e0a2607d8848eeba08ad4f36d0ffd
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-16 22:53:05 +0600

    updated code gradientDescent.m

--- Octave Codes/Week2-Programming-Assignments/gradientDescent.m
+++ Octave Codes/Week2-Programming-Assignments/gradientDescent.m
@@ -1,9 +1,5 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 
-
-%   taking num_iters gradient steps with learning rate alpha
-
-% Initialize some useful values
 m = size(X,1); % number of training examples
 J_history = zeros(num_iters, 1);
 
