78c81bef54e0df6845814298a3c603183cab2095
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-16 22:52:01 +0600

    updated code computeCost.m

--- Octave Codes/Week2-Programming-Assignments/computeCost.m
+++ Octave Codes/Week2-Programming-Assignments/computeCost.m
@@ -3,17 +3,12 @@ function J = computeCost(X, y, theta)
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
+
 m = size(X,1);
 predictions = X*theta ;
 sqrErrors = (predictions - y).^2;
 
 J = 1/(2*m) * sum(sqrErrors);
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
-
-
-
-
 
 % =========================================================================
 
