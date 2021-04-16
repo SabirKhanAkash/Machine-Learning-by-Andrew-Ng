8797a7bb272045e1bdc67c89072bd832d7f10127
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-16 22:54:11 +0600

    updated code gradientDescent.m

--- Octave Codes/Week2-Programming-Assignments/gradientDescent.m
+++ Octave Codes/Week2-Programming-Assignments/gradientDescent.m
@@ -6,29 +6,19 @@ J_history = zeros(num_iters, 1);
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
 
 
 
 
 
     
-    % ============================================================
-
-    % Save the cost J in every iteration    
+    % ============================================================   
     
     predictions = X*theta ;
     sqrErrors = (predictions - y);
 
     J = 1/(m) * X' * sqrErrors;
     theta = theta - alpha*J;
-    %J_history(iter) = computeCost(X, y, theta);
 
 end
 
