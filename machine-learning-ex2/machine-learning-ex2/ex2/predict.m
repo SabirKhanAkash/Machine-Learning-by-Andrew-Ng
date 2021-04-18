3bda7d44db7cf9eeb591061ac9412fff8d0126b7
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-19 03:20:42 +0600

    predict.m updated

--- machine-learning-ex2/machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/machine-learning-ex2/ex2/predict.m
@@ -15,10 +15,9 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+theta1 = sigmoid(X*theta);
 
-
-
-
+p = theta1 >= 0.5;
 
 
 % =========================================================================
