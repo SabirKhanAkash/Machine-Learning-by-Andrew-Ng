f1b3aaba3303426c16b028ac4b9a3b42f2620671
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-19 03:22:40 +0600

    code updated

--- machine-learning-ex2/machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/machine-learning-ex2/ex2/predict.m
@@ -6,10 +6,6 @@ m = size(X, 1);
 p = zeros(m, 1);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters. 
-%               You should set p to a vector of 0's and 1's
-%
 
 theta1 = sigmoid(X*theta);
 
