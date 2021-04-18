4c66e461d82e08b20e4b182c8f72445a4a58f160
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-19 03:03:40 +0600

    costFunction for logistictic regression added

--- machine-learning-ex2/machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/machine-learning-ex2/ex2/costFunction.m
@@ -21,10 +21,22 @@ grad = zeros(size(theta));
 %
 
 
+z=X*theta;
+h=sigmoid(z);
 
+H=h.';
+h1=log(H)*(-y);
+h2=(log(1-H))*(1-y);
+J=(h1-h2)/m;
+s=h-y;
+s=s.';
 
+for i=1:3,    
+    temp=(s)*X(:,i);
+    grad(i)=temp(1);
+end
 
-
+grad=grad./m;
 
 
 % =============================================================
