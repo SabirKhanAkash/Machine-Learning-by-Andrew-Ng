8321bd9e42b830f72deafb0cf567e90f8997f291
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-19 03:23:08 +0600

    code updated

--- machine-learning-ex2/machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/machine-learning-ex2/ex2/predict.m
@@ -1,8 +1,6 @@
 function p = predict(theta, X)
 
 m = size(X, 1);
-
-% You need to return the following variables correctly
 p = zeros(m, 1);
 
 % ====================== YOUR CODE HERE ======================
@@ -11,8 +9,6 @@ theta1 = sigmoid(X*theta);
 
 p = theta1 >= 0.5;
 
-
 % =========================================================================
-
 
 end
