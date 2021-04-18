bd1388c5a4fa673c1a075c83d4bb04ee567ffd12
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-19 03:09:28 +0600

    code updated

--- machine-learning-ex2/machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/machine-learning-ex2/ex2/costFunction.m
@@ -5,10 +5,6 @@ J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
 %
 % Note: grad should have the same dimensions as theta
 %
