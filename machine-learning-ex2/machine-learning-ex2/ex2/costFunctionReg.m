807f8ab672e917aada780981144ef931ff90ab25
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-04-20 02:23:43 +0600

    costFunctionReg.m code updated

--- machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
@@ -1,4 +1,4 @@
-function [J, grad] = costFunction(theta, X, y)
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 m = length(y);
 J = 0;
@@ -11,6 +11,7 @@ h=sigmoid(X*theta);
 H=h.';
 h1=log(H)*(-y);
 h2=(log(1-H))*(1-y);
+theta(1) = 0;
 
 rcost = (lambda / (2 * m)) * (theta'*theta);
 J=((h1-h2)/m)+rcost;
