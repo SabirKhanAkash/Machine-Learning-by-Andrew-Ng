f6d920b5cdfb180019f384766ae5fb7415f93da1
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-05-17 00:49:05 +0600

    lrcostFunction.m updated

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -1,4 +1,4 @@
-function [J, grad] = lrCostFunction(theta, X, y, lambda)
+function [J, grad1] = lrCostFunction(theta, X, y, lambda)
 %LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
 %regularization
 %   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
@@ -13,39 +13,19 @@ J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Hint: The computation of the cost function and gradients can be
-%       efficiently vectorized. For example, consider the computation
-%
-%           sigmoid(X * theta)
-%
-%       Each row of the resulting matrix will contain the value of the
-%       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
-%
-% Hint: When computing the gradient of the regularized cost function, 
-%       there're many possible vectorized solutions, but one solution
-%       looks like:
-%           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
-%           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
 
+z=X*theta;
+h=sigmoid(z);
 
+H=h.';
+h1=log(H)*(-y);
+h2=(log(1-H))*(1-y);
+n = lambda*sum(theta(2:size(theta)).^2)/(2*m);
+J=((h1-h2)/m)+n;
 
+grad(1) = 1/m*(X(:,1)'*(sigmoid(X*theta)-y));
 
-
-
-
-
-
-
-% =============================================================
+grad(2:end) = 1/m*(X(:,2:end)'*(sigmoid(X*theta)-y))+(lambda/m)*theta(2:end);
 
 grad = grad(:);
 
